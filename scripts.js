fe916f6d52adf5595c6bc1e7a6c04ea980caffb6
VRX-M3 <mahmoudmegdad3@gmail.com>
2024-08-17 03:18:31 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('toggleSidebar').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar.style.transform === 'translateX(0%)') {
+        sidebar.style.transform = 'translateX(-100%)';
+    } else {
+        sidebar.style.transform = 'translateX(0%)';
+    }
+});
